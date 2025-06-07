489ef771de720e91049bd84383718ea468256ca7
omochanocha <yokpef@gmail.com>
2025-06-07 18:20:21 +0900

    prettierのupdateとlefthookの導入

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
+  singleQuote: true,
+  // plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
